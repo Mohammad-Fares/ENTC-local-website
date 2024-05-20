7c43f0ee393d80242e89b9bb9594514521766af4
Mohammad-Fares <163373532+Mohammad-Fares@users.noreply.github.com>
2024-05-20 23:26:45 +1000

    completed responsive navbar
    completed making the navbar responsive for all pages

--- home.js
+++ home.js
@@ -1,3 +1,14 @@
+function menu() {
+    var container = document.querySelector(".container");
+        var nav = document.querySelector(".nav");
+        if (container.style.display === "none") {
+            container.style.display = "inline-block";
+            nav.style.borderRadius = "1.5em 1.5em 0 0";
+        } else {
+            container.style.display = "none";
+            nav.style.borderRadius = "3em"; 
+        }
+}
 function confirm() {
     alert("Your form has been submitted")
-  }
+  }yle.display
